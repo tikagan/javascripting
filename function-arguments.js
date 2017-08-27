81be4677f907c05f486c1033b00defc6e8a0ac8c
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-08-27 19:52:13 +0000

    last bit of javascripting

---
+++ function-arguments.js
@@ -0,0 +1,6 @@
+function math (x, y, z) {
+  var result = x + (y * z);
+  return result;
+}
+
+console.log(math(53, 61, 67));
---
+++ function.js
@@ -0,0 +1,7 @@
+function eat (x){
+    food = [x];
+    String(food);
+    return food + ' tasted really good.';
+ }
+
+console.log(eat("bananas"));
